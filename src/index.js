1197896cad78bdd89152d09f97464bdf6789ac3d
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-02-26 09:19:32 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import APIPoliciesDemo from './api-policies'; // Import your component
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <APIPoliciesDemo />
+  </React.StrictMode>
+);
