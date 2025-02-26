e248a8fa47b84f1d7eb5a5d9b8f7e8c31ed3f1a2
Ed Mikuszewski <emikuszewski@pivotal.io>
2025-02-26 09:51:55 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import APIPoliciesDemo from './api-policies'; // Import your component
+import APIPoliciesDemo from './api-policies';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
